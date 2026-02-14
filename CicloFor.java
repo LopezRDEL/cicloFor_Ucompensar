6014d51aa00c078b100c2aa05105b7667acf94a1
LopezRDEL <141454267+LopezRDEL@users.noreply.github.com>
2026-02-13 21:28:25 -0500

    Add files via upload
    Se implementa el condicional ¨if¨ para acumular cuantos números negativos y negativos fueron ingresados dentro del sistema

--- CicloFor.java
+++ CicloFor.java
@@ -12,7 +12,7 @@ public class CicloFor {
         Scanner lector = new Scanner (System.in);
         
         //Declaracion de variables
-        int sum=0, i, num=0;
+        int sum=0, i, num=0, nn=0, pn=0;
         
         //Como es promedio (Division) sera de dato float
         float promedio;
@@ -26,6 +26,13 @@ public class CicloFor {
         // El num que inicia en 0 ira acumulandolos y guardandolos en el lector (Scanner)
             num = lector.nextInt();
             
+            //Iniciacion condicional if para sumar cuantos numeros positivos y negativos hay
+            if (num<0) {
+                nn++;
+            } else { 
+                pn++;
+            }
+            
             //Imprimir la suma de todos los numeros    
             sum = sum + num;   
                }
@@ -38,5 +45,17 @@ public class CicloFor {
         
         //Imprimir los numeros insertados 
         System.out.println("El promedio de ellos es " + promedio );
+        
+        //Imprimir cuantos numeros negativos hay 
+        
+        System.out.println("Los numeros negativos ingresados fueron " + nn);
+        
+        //Imprime cuantos numeros positivos se ingresaron
+        
+        System.out.println("Los numeros positivos ingresados son " + pn);
+        
+        
+        
     }
 }
+ 
